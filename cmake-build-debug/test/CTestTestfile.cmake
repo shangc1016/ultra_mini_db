b3e87e114c492b3d6bc2c692e20d135e27382d30
shangc1016 <shangc1016@163.com>
2022-10-21 09:38:45 +0800

    rm config dir
    1. rm clangd .cache dir
    2. rm idea .idea dir
    3. rm cmake cmake-build-debug dir

--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sc/dev/cDev/ultra_mini_db/test
-# Build directory: /home/sc/dev/cDev/ultra_mini_db/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
